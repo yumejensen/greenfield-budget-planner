28d9f6211c098d52da569c6b377b843465867d7b
joseph <josephpfurman@gmail.com>
2025-08-26 15:09:44 -0500

    Setup in progress

---
+++ src/client/components/Landing.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Landing(){
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  )
+};
+
+export default Landing;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,36 @@
-// const path = require('path');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-// module.exports = {
-//   mode: 'development',
-//   entry: '',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: '',
-//   },
-// };
+module.exports = {
+  mode: "development",
+  entry: "./src/client/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
 
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(?:js|mjs|cjs|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
