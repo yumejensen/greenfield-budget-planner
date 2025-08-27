1d2d40ad68d590bb80fed86abd8e7da959d502a7
avaAlley <ava.rose.alley@gmail.com>
2025-08-27 11:44:51 -0500

    added auth file for passport and google login

---
+++ src/server/auth.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const User = require('./db/index.js');
+
+require('dotenv').config();
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/itineraries",
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
