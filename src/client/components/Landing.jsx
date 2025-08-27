0c2868d6fb32555f584d0aa7d211cb933e25f483
avaAlley <ava.rose.alley@gmail.com>
2025-08-27 11:44:30 -0500

    Added to the Landing page for redirect to login

--- src/client/components/Landing.jsx
+++ src/client/components/Landing.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Landing(){
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>Welcome to your travel budget planner!</h1>
+      <a href='/auth/google'>Click here to sign in with google!</a>
     </div>
   )
 };
