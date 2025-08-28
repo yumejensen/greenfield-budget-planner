feb3c9d630a0262fff0d19536f04626c8309eee1
yumejensen <yumejensen@gmail.com>
2025-08-28 15:24:13 -0500

    Fixed passport sign-in! Using oauth 20 instead of 2, verified user is directed inside app, cookies are made

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const session = require('express-session');
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 const { User } = require('./db/index.js');
 
 require('dotenv').config();
@@ -11,9 +11,8 @@ passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback",
-    passReqToCallback: true
   },
-  function(request, accessToken, refreshToken, profile, done) {
+  function(accessToken, refreshToken, profile, done) {
     // use findOneAndUpdate to initialize or create a user
     // takes in: what you're looking for (profile id), what needs to be updated, options: different filters
     User.findOneAndUpdate({ 
@@ -25,10 +24,8 @@ passport.use(new GoogleStrategy({
     }, { 
       new: true, upsert: true 
     })
-    .then((profile) => {
-      // console.log('hello - this is the then block in findOneAndUpdate');
-      // console.log('profile is', profile)
-      done();
+    .then((user) => {
+      done(null, user);
     })
     .catch((err) => {
       console.log('failed to find and update user PASSPORT:', err)
@@ -40,9 +37,6 @@ passport.use(new GoogleStrategy({
 passport.serializeUser(function(user, cb) {
   process.nextTick(function() {
     return cb(null, {
-      // id: user.id,
-      // username: user.username,
-      // picture: user.picture
       email: user.email,
       googleId: user.googleId,
       id: user._id
