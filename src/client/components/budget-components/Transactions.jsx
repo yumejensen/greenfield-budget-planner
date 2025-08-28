08ba084fe1cd7ddf5158145b34f4aadf00b5dcd2
avaAlley <ava.rose.alley@gmail.com>
2025-08-28 18:57:28 -0500

    Added Transactions component

---
+++ src/client/components/budget-components/Transactions.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+// import other react components
+import Transaction from "./Transaction";
+
+const Transactions = () => {
+
+  // state
+
+  // any helpers needed
+
+  // return with html
+  return (
+    <div>
+      This will populate with all transactions:
+      <Transaction />
+    </div>
+  )
+}
+
+export default Transactions;
