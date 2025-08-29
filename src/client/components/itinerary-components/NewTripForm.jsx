902e6330d4678fae7d119c7e768c9ee634bcdde5
RRARI504 <ryansimien@ymail.com>
2025-08-29 16:11:47 -0500

    Did nothing to this component yet but added the template bare bones

--- src/client/components/itinerary-components/NewTripForm.jsx
+++ src/client/components/itinerary-components/NewTripForm.jsx
@@ -1,1 +1,21 @@
 import React from 'react';
+import { useState } from "react";
+
+const NewTripForm = (props) => {
+
+  // props
+  const { children = 'default' } = props
+
+  // state can go here if needed
+
+  // any helpers needed
+
+  // return with html
+  return (
+    <div>
+      This is a {children} component
+    </div>
+  )
+}
+
+export default NewTripForm;
