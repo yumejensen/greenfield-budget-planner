a9a568f30bf133207c97e1c1410480c56eb0cb94
yumejensen <yumejensen@gmail.com>
2025-08-29 15:31:14 -0500

    Modified Signin.jsx and CreateEvent.jsx

--- src/client/components/Signin.jsx
+++ src/client/components/Signin.jsx
@@ -1,4 +1,8 @@
 import React from "react"
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Signin(props){
   
@@ -6,19 +10,26 @@ function Signin(props){
   const { userAuth } = props
 
   return (
-    <div>
-      <h1>Welcome to your travel budget planner!</h1>
-      <a href='/auth/google'>
-        <button 
-          type="button" 
-          className="btn btn-outline-info" 
-          onClick={() => {
-            userAuth();
-          }}>
-            Sign-in
-        </button>
-      </a>
-    </div>
+    <Container>
+        <Row>
+          <h1>Travel Chum</h1>
+        </Row>
+        <Row>
+          <p>Your all-in-one travel planning and budgeting buddy!</p>
+        </Row>
+        <Row>
+          <a href='/auth/google'>
+            <Button variant="primary"
+              type="button" 
+              className="btn btn-outline-info" 
+              onClick={() => {
+                userAuth();
+              }}>
+                Sign-in
+            </Button>
+          </a>
+        </Row>
+    </Container>
   )
 }
 
---
+++ src/client/components/event-components/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+function Calendar() {
+  return (
+    <Container>
+        Calendar goes here
+    </Container>
+  );
+}
+
+export default Calendar;
