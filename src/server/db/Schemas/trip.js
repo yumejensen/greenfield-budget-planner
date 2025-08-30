7c90d86efb8bd19569434038040d01b43845ef3a
yumejensen <yumejensen@gmail.com>
2025-08-30 16:13:13 -0500

    Updated User schema, added Trip and Event schema

---
+++ src/server/db/Schemas/trip.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const tripSchema = new Schema({
+    // user to trips is a 1:n 
+    user: { type: Schema.Types.ObjectId, ref: 'User'},
+    // trip to events is a 1:n 
+    events : [{type: Schema.Types.ObjectId, ref: 'Event'}],
+    title: String,
+    start: Date,
+    end: Date,
+    nativeCurrency: String,
+    foreignCurrency: String
+});
+
+
+module.exports.tripSchema = tripSchema;
