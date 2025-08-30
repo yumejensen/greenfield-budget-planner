b1ee81f9dbcc4928cc9c6056f0a9f5b1934b2b6b
avaAlley <ava.rose.alley@gmail.com>
2025-08-30 12:19:24 -0500

    Added currency dropdown for testing

--- src/client/components/budget-components/BudgetTracker.jsx
+++ src/client/components/budget-components/BudgetTracker.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import BudgetBar from "./BudgetBar.jsx";
 import AddTransaction from "./AddTransaction.jsx";
 import Transactions from "./Transactions.jsx";
+import CurrencyDropDown from "./CurrencyDropDown.jsx";
 
 function BudgetTracker(){
 
@@ -21,6 +22,7 @@ function BudgetTracker(){
   return (
     <div>
       <h1>Budget Tracker</h1>
+      <CurrencyDropDown />
       <BudgetBar />
       <AddTransaction />
       <Transactions />
