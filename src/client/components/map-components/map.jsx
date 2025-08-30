2ef5c64bd4b0444047eb0c7df9a138d42cd66e16
joespaf <josephpfurman@gmail.com>
2025-08-29 20:22:23 -0500

    Added functioning Map embedd. Required mapbox-gl to be added to the package. Will destroy token after finishing work on this project.

--- src/client/components/map-components/map.jsx
+++ src/client/components/map-components/map.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 
-function Map(){
-  return(
-    <div>
-      Map Component goes here!
-    </div>
+const MAPBOX_API_TOKEN = "pk.eyJ1Ijoiam9lc3BhZiIsImEiOiJjbWJwZzd4M2UwNGZtMnhvZmxzMzd0YzM4In0.cghf9WgnsnrcBinMF4xBSg"
+
+function Map() {
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+  useEffect(() => {
+    mapboxgl.accessToken = MAPBOX_API_TOKEN
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
+    return (
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
   )
-};
+}
 
 export default Map;
