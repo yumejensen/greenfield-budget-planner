6829d5fca294239f858466deccc1aee1b0ea8df0
joseph <josephpfurman@gmail.com>
2025-08-31 15:06:24 -0500

    Seed command now runs and exits, seems to function as expected

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -7,23 +7,23 @@ const {
 } = require('./index')
 
 
-const seedDB = new Promise((resolve, reject)=>{
-  Promise.all([
-    User.create({
-      email: 'abrahamlincoln@us.gov',
-      googleId: 'bestPrezEver'
-    }),
-    
+async function seedDB(){
+  await User.create({
+    email: 'abrahamlincoln@us.gov',
+    googleId: '1234'
+  })
+  const abe = await User.findOne({
+    email: 'abrahamlincoln@us.gov'
+  })
+  console.log('Abe found :)', abe)
+}
 
-  ])
-
-  
-
-
-})
-
-
-const abe = User.findOne({email: 'abrahamlincoln@us.gov'})
-
-console.log(abe)
-
+seedDB()
+  .then(()=>{
+    console.log('Database seeded successfully')
+    process.exit(0)
+  })
+  .catch((err)=>{
+    console.log('Error seeding Database', err)
+    process.exit(1)
+  })
