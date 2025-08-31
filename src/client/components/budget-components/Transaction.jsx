02473e170619b9fae5eca2c118f874b5b828f730
avaAlley <ava.rose.alley@gmail.com>
2025-08-31 15:49:17 -0500

    Dynamically renders sample data

--- src/client/components/budget-components/Transaction.jsx
+++ src/client/components/budget-components/Transaction.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Accordion } from "react-bootstrap";
 
-const Transaction = () => {
+const Transaction = (props) => {
+
+  // destructure props
+  const { amount, currencyName, details, createdAt } = props.transaction;
 
   // return with html
   return (
-    <div>
-      This will populate with a transaction.
-    </div>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item>
+        <Accordion.Header>{amount} - {currencyName}</Accordion.Header>
+        <Accordion.Body>
+          {details} - {createdAt}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   )
 }
 
