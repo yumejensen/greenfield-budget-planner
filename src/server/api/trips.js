fc553428c7e482ade577d169460b941e712f8923
RRARI504 <ryansimien@ymail.com>
2025-09-01 07:17:36 -0500

    Set up API component

---
+++ src/server/api/trips.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+
+// ---------------ROUTER----------------
+
+const Trips = Router();
+
+// -------------HELPERS-----------------
+
+
+
+module.exports = {
+  Trips,
+};
