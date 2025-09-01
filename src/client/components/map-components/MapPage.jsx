4d70a7f73a213066873f5fc1c1da3882da677d04
joseph <josephpfurman@gmail.com>
2025-09-01 08:09:31 -0500

    map component linked to DB

--- src/client/components/map-components/MapPage.jsx
+++ src/client/components/map-components/MapPage.jsx
@@ -16,9 +16,20 @@ const MapPage = (props) => {
       });
   };
 
+  const sampleMapData = () => {
+    axios.post('/api/map', { name: 'Sample POI', coords: [0, 0] })
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.error('Error fetching map data:', err);
+      });
+  };
+
   return (
     <div>
       This is the Map Page
+      <button onClick={sampleMapData}>Add Sample Map Data</button>
       <button onClick={fetchMapData}>Fetch Map Data</button>
       <Map />
     </div>
--- src/server/api/map.js
+++ src/server/api/map.js
@@ -2,14 +2,44 @@ const { Router } = require('express');
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-
-// ---------------ROUTER----------------
-
+const { POI } = require('../db/index.js');
 const Map = Router();
 
-// -------------HELPERS-----------------
 
+Map.get('/', (req, res) => {
+  console.log('POI query');
+  console.log(req.user);
+  POI.find({ userid: req.user.id })
+    .then((pois) => {
+      const clean = pois.map((poi) => {
+        // obscure _id and userid
+        return { name: poi.name, coords: poi.coords, event: poi.event};
+      });
+      res.json(clean);
+    })
+    .catch((err) => {
+      console.error('Error fetching POIs:', err);
+      res.status(500).json({ error: 'Server Error' });
+    });
+});
 
+Map.post('/', (req, res) => {
+  console.log('POI creation attempt');
+  if (!req.user || !req.user.id) {
+    console.log('no user');
+    return res.status(401).json({ error: 'log in, bud' });
+  }
+  const { name, coords } = req.body;
+  POI.create({ name, coords, userid: req.user.id })
+    .then((newPOI) => {
+      console.log('POI created:', newPOI);
+      res.status(201).json(newPOI);
+    })
+    .catch((err) => {
+      console.error('Error creating POI:', err);
+      res.status(500).json({ error: 'Server Error' });
+    });
+});
 
 module.exports = {
   Map,
