0320e9678db3e7683544e85adc278fadf671a095
RRARI504 <ryansimien@ymail.com>
2025-09-01 07:21:33 -0500

    fixed form and began setting up state for form

--- src/client/components/itinerary-components/NewTripForm.jsx
+++ src/client/components/itinerary-components/NewTripForm.jsx
@@ -1,33 +1,70 @@
+//import react and react hooks {use state}
 import React from 'react';
-import { useState } from "react";
+// import { useState } from "react";
+
+//import bootstrap components button and form.
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function NewTripForm() {
+//make a function takes in prop onAddTrip
+function NewTripForm({ onAddTrip }) {
+
+  //initializing state object for form inputs
+  // const [formData, setFormData] = useState({
+  //   title: '',
+  //   start: '',
+  //   end: '',
+  //   nativeCurrency: '',
+  //   foreignCurrency: '',
+  //   budget: '',
+  // });
+
+  //supposed to update the state with changes from form
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData(prev => ({ ...prev, [name]: value }));
+  // };
+
+//   const handleSubmit = (e) => {
+
+// }
+
   return (
-    <Form>
-
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>New Trip</Form.Label>
-        <Form.Control type="email" placeholder="Where to next?" />
-        <Form.Text className="text-muted">
-        </Form.Text>
+    <Form className="mb-4">
+      <Form.Group className="mb-3">
+        <Form.Label>Trip Title</Form.Label>
+        <Form.Control name="title" type="text" placeholder="Enter trip title"  />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3">
+        <Form.Label>Start Date</Form.Label>
+        <Form.Control name="start" type="date"  />
       </Form.Group>
 
-      {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group> */}
+      <Form.Group className="mb-3">
+        <Form.Label>End Date</Form.Label>
+        <Form.Control name="end" type="date" />
+      </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Add New Trip
-      </Button>
+      <Form.Group className="mb-3">
+        <Form.Label>Native Currency</Form.Label>
+        <Form.Control name="nativeCurrency" type="text"/>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Foreign Currency</Form.Label>
+        <Form.Control name="foreignCurrency" type="text"  />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Budget</Form.Label>
+        <Form.Control name="budget" type="number"  />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">Add Trip</Button>
     </Form>
   );
 }
+
 
 export default NewTripForm;
